1f1290f399b138cf44e5cc631bdde84dc9956a87
codeLemur <lars.kipfer55@gmail.com>
2023-03-09 22:12:29 +0100

    New: Add globals with constants

---
+++ globals.py
@@ -0,0 +1,9 @@
+# Database Keys
+ROLE_KEY = 'role'  # Possible roles: start, goal, server
+TIMESTAMP_KEY = 'time'
+START_NUMBER_KEY = 'start_number'
+
+# Roles
+ROLE_START = 'start'
+ROLE_GOAL = 'goal'
+ROLE_SERVER = 'server'
