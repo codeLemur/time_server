d8f222f1c6a10c51478d10c66d823566cf915bd8
codeLemur <lars.kipfer55@gmail.com>
2023-03-14 21:40:18 +0100

    Mod: Use SM in main application - React on state changes - React on time reportings - Return current state for GET requests

--- app_server.py
+++ app_server.py
@@ -3,25 +3,32 @@ from flask import Flask, request
 
 import data_handler
 import globals
+import state_machine
 
 app = Flask(__name__)
 dh = data_handler.DataHandler()
+sm = state_machine.StateMachine()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    result = -1
     if request.method == 'POST':
         data = request.get_json()
-        # Do something with the data
         logging.info(f'Received POST request with data: {data}')
-        dh.save_timestamp(data)
-        if data[globals.ROLE_START]:
-            dh.set_last_start_time(data)
-        elif data[globals.ROLE_GOAL]:
-            dh.set_last_goal_time(data)
-            dh.calculate_race_duration()  # TODO move to other location, wait for confirmation if signal counts
+        if data[globals.COMMAND_TYPE_KEY] == globals.CMD_STATE_CHANGE:
+            handle_state_change_command(data)
+            result = 0
+        elif data[globals.COMMAND_TYPE_KEY] == globals.CMD_REPORT_TIME:
+            handle_report_time_command(data)
+            result = 0
+        else:
+            logging.error(f'Received invalid command: {data[globals.COMMAND_TYPE_KEY]}')
 
-        return 'Received POST request with data: {}'.format(data)
+        return result  # 0 for success, -1 for error
+    elif request.method == 'GET':
+        logging.info('Received GET request, return current State')  # TODO remove to speed this up
+        return sm.get_current_state().value
     else:
         return 'Hello, world!'
 
@@ -29,6 +36,23 @@ def index():
 @app.route('/')
 def hello():
     return 'Hello, world!'
+
+
+def handle_state_change_command(data: dict):
+    if sm.update(data[globals.EVENT_KEY]):
+        # State changed do entry action for specific states
+        if sm.get_current_state() == globals.States.STOPPED:
+            dh.calculate_race_duration()
+
+
+def handle_report_time_command(data: dict):
+    dh.save_timestamp(data)
+    if globals.ROLE_KEY in data:
+        if data[globals.ROLE_START]:
+            dh.set_last_start_time(data)
+        elif data[globals.ROLE_GOAL]:
+            dh.set_last_goal_time(data)
+            dh.calculate_race_duration()
 
 
 if __name__ == '__main__':
