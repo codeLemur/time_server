9e2ffe08d1179ce9fbdae000592aaa950f2c5834
codeLemur <lars.kipfer55@gmail.com>
2023-03-15 20:42:54 +0100

    Mod: Retrun False when command was not successful

--- app_server.py
+++ app_server.py
@@ -17,11 +17,11 @@ def index():
         data = request.get_json()
         logging.info(f'Received POST request with data: {data}')
         if data[globals.COMMAND_TYPE_KEY] == globals.CMD_STATE_CHANGE:
-            handle_state_change_command(data)
-            result = 'success'
+            if handle_state_change_command(data):
+                result = 'success'
         elif data[globals.COMMAND_TYPE_KEY] == globals.CMD_REPORT_TIME:
-            handle_report_time_command(data)
-            result = 'success'
+            if handle_report_time_command(data):
+                result = 'success'
         else:
             logging.error(f'Received invalid command: {data[globals.COMMAND_TYPE_KEY]}')
 
@@ -38,32 +38,39 @@ def hello():
     return 'Hello, world!'
 
 
-def handle_state_change_command(data: dict):
+def handle_state_change_command(data: dict) -> bool:
+    result = False
     event = globals.Events[data[globals.EVENT_KEY]]
     # TODO check for roles?
     if sm.update(event):
+        result = True
         # State changed do entry action for specific states
         if sm.get_current_state() == globals.States.STOPPED:
             dh.calculate_race_duration()
 
+    return result
 
-def handle_report_time_command(data: dict):
+
+def handle_report_time_command(data: dict) -> bool:
+    result = False
     data.pop(globals.COMMAND_TYPE_KEY)
     dh.save_timestamp(data)
     if globals.ROLE_KEY in data:
         if data[globals.ROLE_KEY] == globals.ROLE_START:
             if sm.get_current_state() == globals.States.READY:
                 dh.set_last_start_time(data)
+                result = True
             else:
                 logging.error(f'Timestamp is not stored because in wrong state: {sm.get_current_state().name}')
         elif data[globals.ROLE_KEY] == globals.ROLE_GOAL:
             if sm.get_current_state() == globals.States.RUNNING:
                 dh.set_last_goal_time(data)
+                result = True
             else:
                 logging.error(f'Timestamp is not stored because in wrong state: {sm.get_current_state().name}')
-                # TODO return error value
         else:
             logging.error(f'Invalid role: {data[globals.ROLE_KEY]}')
+    return result
 
 
 if __name__ == '__main__':
